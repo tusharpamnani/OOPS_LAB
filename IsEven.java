0dd35dac437986302f21813b877a10a18821aabc
Tushar Pamnani <121151091+tusharpamnani@users.noreply.github.com>
2023-05-02 11:55:22 +0530

    Create IsEven.java

---
+++ IsEven.java
@@ -0,0 +1,3 @@
+/*
+Write a java program to declare a method called "IsEven" that takes an integer parameter and returns true if the number is even and false optherwise. And then call the method with several different values and print the result to the console.(Use Scanner class)
+/*
